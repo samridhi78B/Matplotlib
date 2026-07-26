5be4ab31f77273c23b52ab61fcf18990b17416d4
Samridhi Gumber <samridhigumber05@gmail.com>
2026-07-26 20:39:29 +0530

    Create scatter_plot.py

---
+++ graphs/scatter_plot.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+time= [2,1,3,4,]
+scores =[45,22,80,98]
+
+plt.scatter(time,scores, color='green',marker='o', label='Scores')
+plt.legend()
+plt.xlabel('Time')
+plt.ylabel('scores')
+plt.title('Comparion of students')
+plt.grid()
+plt.show()
